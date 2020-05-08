878e5f92497c386cc9550f4d565f1ded7e8f9a11
Guillaume Ballet <gballet@gmail.com>
2020-05-08 22:10:28 +0200

    Rely on jupiter-account for TxData and Tx

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,17 @@ extern crate jupiter_account;
 extern crate multiproof_rs;
 extern crate rlp;
 
-use jupiter_account;
-use jupiter_account::Account;
 
+use jupiter_account::{Account, Tx, TxData};
+
+use multiproof_rs::{Multiproof, Node, ProofToTree, Tree};
+
+fn verify(txdata: &[u8]) -> Result<bool, &str> {
+    return Err("not implemented");
+}
+
+fn update<K>(trie: &dyn Tree<K>, from: &Account, to: &Account) -> Vec<u8> {
+}
 
 #[cfg(not(test))]
 #[no_mangle]
