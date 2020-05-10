0811f7cbd3c11b3a37c0645de1a03670fbff0a56
Guillaume Ballet <gballet@gmail.com>
2020-05-10 12:47:05 +0200

    Outline of the precompiles
    Nonce and signature checks are still missing

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,105 @@ extern crate jupiter_account;
 extern crate multiproof_rs;
 extern crate rlp;
 
+extern "C" {
+    pub fn revert();
+    pub fn finish(data: *const u8, len: usize);
+    pub fn calldata(buf: *const u8, offset: u64, len: usize);
+    pub fn calldata_size() -> usize;
 
-use jupiter_account::{Account, Tx, TxData};
-
-use multiproof_rs::{Multiproof, Node, ProofToTree, Tree};
-
-fn verify(txdata: &[u8]) -> Result<bool, &str> {
-    return Err("not implemented");
+    pub fn get_storage_root() -> Vec<u8>;
 }
 
-fn update<K>(trie: &dyn Tree<K>, from: &Account, to: &Account) -> Vec<u8> {
+use jupiter_account::{Account, TxData};
+
+use multiproof_rs::{Node, ProofToTree, Tree};
+
+fn verify(txdata: &TxData) -> Result<Node, String> {
+    let trie: Node = txdata.proof.rebuild()?;
+
+    // Check that the hash is the same as the root's
+    // storage
+    if trie.hash() != unsafe { get_storage_root() } {
+        return Err(format!(
+            "Storage and proof root hashes differ: {:?} != {:?}",
+            unsafe { get_storage_root() },
+            trie.hash()
+        ));
+    }
+
+    // Check that all txs' from addresses are in the trie
+    for tx in txdata.txs.iter() {
+        if !trie.has_key(&tx.from) {
+            return Err(format!("key {:?} isn't in trie", tx.from));
+        }
+    }
+
+    Ok(trie)
+}
+
+fn update(trie: &mut Node, from: &Account, to: &Account) -> Vec<u8> {
+    match (from, to) {
+        (Account::Existing(fa, _, _, _, _), Account::Existing(ta, _, _, _, _)) => {
+            trie.insert(fa, rlp::encode(from)).unwrap();
+            trie.insert(ta, rlp::encode(to)).unwrap();
+
+            return trie.hash();
+        }
+        _ => panic!("Updated accounts can't be empty"),
+    }
 }
 
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn main() {
+    let mut payload = vec![0u8; unsafe { calldata_size() }];
+    unsafe {
+        calldata(payload.as_mut_ptr(), 0, calldata_size());
+    }
+    let txdata: TxData = rlp::decode(&payload).unwrap();
+    let res = Vec::new();
+
+    if let Ok(mut trie) = verify(&txdata) {
+        for tx in txdata.txs {
+            if let Node::Leaf(_, ref f) = trie[&tx.from] {
+                let mut from = rlp::decode::<Account>(&f).unwrap();
+                if let Node::Leaf(_, ref t) = trie[&tx.to] {
+                    let mut to = rlp::decode::<Account>(&t).unwrap();
+
+                    if from.balance() >= tx.value {
+                        let from_balance = from.balance_mut().unwrap();
+                        *from_balance -= tx.value;
+                        let to_balance = to.balance_mut().unwrap();
+                        *to_balance += tx.value;
+                        update(&mut trie, &from, &to);
+                    } else {
+                        unsafe {
+                            revert();
+                        }
+                    }
+                } else {
+                    // Creation, value has to be checked,
+                    // which means that I have to make sure
+                    // that precompiles have access to value
+                    let to = Account::Existing(tx.to, 0, tx.value, vec![], false);
+                    if from.balance() >= tx.value {
+                        update(&mut trie, &from, &to);
+                    } else {
+                        unsafe {
+                            revert();
+                        }
+                    }
+                }
+            }
+        }
+
+        unsafe {
+            finish(res.as_ptr(), res.len());
+        }
+    }
+    unsafe {
+        revert();
+    }
 }
 
 #[cfg(test)]
