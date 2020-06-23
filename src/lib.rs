bb87eef8196bf2f336038479f2387f160425fa74
Guillaume Ballet <gballet@gmail.com>
2020-06-23 20:09:16 +0200

    fix: channel opening to the contract address

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ mod eth {
         pub fn eei_calldata_size() -> usize;
 
         pub fn eei_get_storage_root(ptr: *mut u8, len: usize);
+        pub fn eei_set_storage_root(ptr: *const u8, len: usize);
     }
 
     pub fn revert() {
@@ -42,6 +43,12 @@ mod eth {
     pub fn get_storage_root(buf: &mut Vec<u8>) {
         unsafe {
             eei_get_storage_root(buf.as_mut_ptr(), buf.len());
+        }
+    }
+
+    pub fn set_storage_root(buf: Vec<u8>) {
+        unsafe {
+            eei_set_storage_root(buf.as_ptr(), buf.len());
         }
     }
 }
@@ -117,10 +124,18 @@ fn verify(txdata: &TxData) -> Result<Node, String> {
     Ok(trie)
 }
 
+fn account_from_trie(key: NibbleKey, trie: &Node) -> Result<Account, &'static str> {
+    if let Node::Leaf(_, ref acc_rlp) = trie[&key] {
+        rlp::decode::<Account>(&acc_rlp).map_err(|_| "RLP error")
+    } else {
+        Err("account doesn't exist")
+    }
+}
+
 fn execute_tx(
     from: &mut Account,
     to: &mut Account,
-    contract: &mut Account,
+    trie: &Node,
     tx: &Tx,
 ) -> Result<(), &'static str> {
     if let Account::Existing(_, ref mut fnonce, ref mut fbalance, _, ref mut fstate) = from {
@@ -167,12 +182,13 @@ fn execute_tx(
 
                     // Check that the data field corresponds to the sender
                     // address that this contract is meant to interface with
+                    let x: Vec<u8> = ByteKey::from(tx.from.clone()).into();
                     let mut keccak256 = Keccak256::new();
-                    keccak256.input::<Vec<u8>>(ByteKey::from(tx.from.clone()).into());
+                    keccak256.input::<Vec<u8>>(x);
                     keccak256.input(&tx.data);
-                    let thisaddr = keccak256.result();
-                    if NibbleKey::from(ByteKey::from(thisaddr.to_vec())) != tx.to {
-                        return Err("H(from, data) != tx.to");
+                    let thisaddr = keccak256.result()[..20].to_vec();
+                    if NibbleKey::from(ByteKey::from(thisaddr)) != tx.to {
+                        return Err("H(tx.from ++ tx.data) != tx.to");
                     }
 
                     // Fund the account
@@ -181,11 +197,11 @@ fn execute_tx(
 
                     // Initialize the state with the destination address,
                     // the origin address and the status byte.
-                    *tstate = vec![0u8; 65];
-                    (*tstate)[..32].copy_from_slice(&tx.data[..32]);
+                    *tstate = vec![0u8; 41];
+                    (*tstate)[..20].copy_from_slice(&tx.data[..20]);
                     let f: Vec<u8> = ByteKey::from(tx.from.clone()).into();
-                    (*tstate)[32..64].copy_from_slice(&f[..]);
-                    // data[64] is therefore set to 0 == "transfer" mode
+                    (*tstate)[20..40].copy_from_slice(&f[..]);
+                    // data[40] is therefore set to 0 == "transfer" mode
                 }
                 // Pay the other contract. The "from" address is the one
                 // of the sender's contract and the "to" address is that
@@ -207,18 +223,15 @@ fn execute_tx(
                     // decrease its balance.
                     *tbalance -= tx.value;
 
-                    // Calculate the senders' contract account so that
-                    // it can be recovered and its balance increased.
-                    if let Account::Existing(ref addr, _, ref mut cbalance, _, _) = contract {
-                        // Check that the address is the right one
-                        let mut keccak256 = Keccak256::new();
-                        keccak256.input(&tstate[32..64]);
-                        keccak256.input(&tstate[..32]);
-                        let thisaddr = keccak256.result();
-                        if &NibbleKey::from(ByteKey::from(thisaddr.to_vec())) != addr {
-                            return Err("receiver address is not authorized by contract");
-                        }
-
+                    // Recover the senders' contract account so that
+                    // its balance can be increased.
+                    let mut keccak256 = Keccak256::new();
+                    keccak256.input(&tstate[32..64]);
+                    keccak256.input(&tstate[..32]);
+                    let thisaddr = keccak256.result();
+                    if let Account::Existing(_, _, ref mut cbalance, _, _) =
+                        account_from_trie(NibbleKey::from(ByteKey::from(thisaddr.to_vec())), trie)?
+                    {
                         // Increase the senders' contract balance.
                         *cbalance += tx.value;
                     } else {
@@ -275,13 +288,13 @@ fn execute_tx(
     }
 }
 
-fn update(trie: &mut Node, from: &Account, to: &Account) -> Vec<u8> {
+fn update(trie: &mut Node, from: &Account, to: &Account) {
     match (from, to) {
         (Account::Existing(fa, _, _, _, _), Account::Existing(ta, _, _, _, _)) => {
             trie.insert(fa, rlp::encode(from)).unwrap();
             trie.insert(ta, rlp::encode(to)).unwrap();
 
-            trie.hash()
+            eth::set_storage_root(trie.hash());
         }
         _ => panic!("Updated accounts can't be empty"),
     }
@@ -291,27 +304,30 @@ fn contract_main() -> Result<Vec<u8>, &'static str> {
     let mut payload = vec![0u8; eth::calldata_size()];
     eth::calldata(&mut payload, 0usize);
     let txdata: TxData = rlp::decode(&payload).unwrap();
-    let res = Vec::new();
 
     if let Ok(mut trie) = verify(&txdata) {
         for tx in txdata.txs {
             if let Node::Leaf(_, ref f) = trie[&tx.from] {
                 let mut from = rlp::decode::<Account>(&f).unwrap();
 
-                let mut to = if let Node::Leaf(_, ref t) = trie[&tx.to] {
-                    rlp::decode::<Account>(&t).unwrap()
+                let mut to = if trie.has_key(&tx.to) {
+                    if let Node::Leaf(_, ref t) = trie[&tx.to] {
+                        rlp::decode::<Account>(&t).unwrap()
+                    } else {
+                        // Creation, value has to be checked.
+                        Account::Existing(tx.to.clone(), 0, 0, vec![], vec![])
+                    }
                 } else {
-                    // Creation, value has to be checked.
-                    Account::Existing(tx.to.clone(), 0, tx.value, vec![], vec![])
+                    Account::Existing(tx.to.clone(), 0, 0, vec![], vec![])
                 };
 
-                execute_tx(&mut from, &mut to, &mut Account::Empty, &tx)?;
+                execute_tx(&mut from, &mut to, &trie, &tx)?;
 
                 update(&mut trie, &from, &to);
             }
         }
 
-        return Ok(res);
+        return Ok(trie.hash());
     }
     Err("could not verify proof")
 }
@@ -329,6 +345,10 @@ pub extern "C" fn main() {
 mod tests {
     use super::multiproof_rs::{make_multiproof, NibbleKey, Node};
     use super::*;
+    use secp256k1::{
+        recover as secp256k1_recover, sign as secp256k1_sign, verify as secp256k1_verify, Message,
+        RecoveryId, SecretKey, Signature,
+    };
 
     #[test]
     fn test_recover_account_no_keys() {
@@ -371,5 +391,129 @@ mod tests {
         let mut r = vec![0u8; 32];
         eth::get_storage_root(&mut r);
         assert_eq!(r, root.hash());
+    }
+    #[test]
+    fn test_channel_creation() {
+        let mut root = Node::default();
+
+        // Create the first account
+        let user1_skey = SecretKey::parse(&[1u8; 32]).unwrap();
+        let msg = Message::parse_slice(&[0x55u8; 32]).unwrap();
+        let (user1_sig, user1_recid) = secp256k1_sign(&msg, &user1_skey);
+        let user1_pkey = secp256k1_recover(&msg, &user1_sig, &user1_recid).unwrap();
+        let mut keccak256 = Keccak256::new();
+        keccak256.input(&user1_pkey.serialize()[..]);
+        let addr1 = keccak256.result_reset()[..20].to_vec();
+        let user1_addr = NibbleKey::from(ByteKey::from(addr1.clone()));
+        root.insert(
+            &user1_addr,
+            rlp::encode(&Account::Existing(
+                user1_addr.clone(),
+                1,
+                1000,
+                vec![],
+                vec![],
+            )),
+        )
+        .unwrap();
+
+        // Create the second account
+        let user2_skey = SecretKey::parse(&[2u8; 32]).unwrap();
+        let (user2_sig, user2_recid) = secp256k1_sign(&msg, &user2_skey);
+        let user2_pkey = secp256k1_recover(&msg, &user2_sig, &user2_recid).unwrap();
+        keccak256.input(&user2_pkey.serialize()[..]);
+        let addr2 = keccak256.result_reset()[..20].to_vec();
+        let user2_addr = NibbleKey::from(ByteKey::from(addr2.clone()));
+        root.insert(
+            &user2_addr,
+            rlp::encode(&Account::Existing(
+                user2_addr.clone(),
+                1,
+                20,
+                vec![],
+                vec![],
+            )),
+        )
+        .unwrap();
+
+        // Intermediate contract address
+        keccak256.input(&addr1);
+        keccak256.input(&addr2);
+        let contract_address =
+            NibbleKey::from(ByteKey::from(keccak256.result_reset()[..20].to_vec()));
+        // Channel-opening layer 2 transaction
+        let mut open_tx = Tx {
+            from: user1_addr.clone(),
+            to: contract_address.clone(),
+            data: addr2,
+            nonce: 1,
+            value: 100,
+            signature: vec![0u8; 65],
+            call: 1,
+        };
+        open_tx.sign(&user1_skey.serialize());
+
+        // Create the proof containing the address of the channel creator,
+        // as well as the addrss of the contract that will be created.
+        let proof =
+            make_multiproof(&root, vec![user1_addr.clone(), contract_address.clone()]).unwrap();
+
+        // Layer 1 tx data
+        let txdata = TxData {
+            proof,
+            txs: vec![open_tx],
+        };
+
+        eth::set_storage_root(root.hash());
+        eth::set_calldata(rlp::encode(&txdata));
+
+        contract_main().unwrap();
+
+        // Check that the final root has been updated
+        // to the proper value
+        let mut newtrie = Node::default();
+        newtrie
+            .insert(
+                &user1_addr,
+                rlp::encode(&Account::Existing(
+                    user1_addr.clone(),
+                    2,
+                    900,
+                    vec![],
+                    vec![],
+                )),
+            )
+            .unwrap();
+        newtrie
+            .insert(
+                &user2_addr,
+                rlp::encode(&Account::Existing(
+                    user2_addr.clone(),
+                    1,
+                    20,
+                    vec![],
+                    vec![],
+                )),
+            )
+            .unwrap();
+        newtrie
+            .insert(
+                &contract_address,
+                rlp::encode(&Account::Existing(
+                    contract_address.clone(),
+                    0,
+                    100,
+                    vec![],
+                    vec![
+                        123, 76, 14, 99, 195, 95, 145, 247, 99, 59, 196, 219, 252, 140, 63, 99, 27,
+                        130, 242, 14, 181, 154, 35, 232, 170, 166, 228, 13, 59, 214, 229, 236, 205,
+                        9, 152, 122, 184, 20, 30, 197, 0,
+                    ],
+                )),
+            )
+            .unwrap();
+        let mut r = vec![0u8; 32];
+        eth::get_storage_root(&mut r);
+        assert_eq!(r, newtrie.hash());
     }
 }
